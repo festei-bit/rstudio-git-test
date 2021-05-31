5f9faf9f733e18662bef1b332fcddbf034bd4c7a
Steinle <Felix.Steinle@sky.de>
2021-05-31 15:26:08 +0200

    first commit in branch

--- base_scripts/test_script.R
+++ base_scripts/test_script.R
@@ -11,7 +11,7 @@ test_data$Species = as.character(test_data$Species)
 
 p = ggplot(data = test_data, aes(x = Species, y= Petal.Width))
 p_2 = p + geom_bar(stat = "identity")
-p_2
+print(p_2)
 
 #ANOTHER TEST LINE
 #c("#B54545", "#FFFFFF", "#FFFFFF")
