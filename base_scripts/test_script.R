311330da787e06d6d07e2e413caa0b7145ab6ee2
festei <f.steinle@gmx.de>
2021-06-01 05:55:23 +0200

    restructuring
    restructured the basic script

--- base_scripts/test_script.R
+++ base_scripts/test_script.R
@@ -9,9 +9,16 @@ test_data$Species = as.character(test_data$Species)
 #THIS IS WHERE DATA WRANGINGLING AND DATABASE CONNECTION STARTS
 #_______________________________________________________________________
 
+
+#ANOTHER TEST LINE
+#_______________________________________________________________________
+#c("#B54545", "#FFFFFF", "#FFFFFF")
+
+
+#VISUALIZATION
+#_______________________________________________________________________
+
 p = ggplot(data = test_data, aes(x = Species, y= Petal.Width))
 p_2 = p + geom_bar(stat = "identity")
 print(p_2)
 
-#ANOTHER TEST LINE
-#c("#B54545", "#FFFFFF", "#FFFFFF")
