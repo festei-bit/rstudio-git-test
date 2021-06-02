0d03eae72b10f0f634ee69ed4078824e63086075
festei <f.steinle@gmx.de>
2021-06-02 11:21:13 +0200

    adjusted header

--- base_scripts/test_script.R
+++ base_scripts/test_script.R
@@ -6,9 +6,8 @@ test_data <- datasets::iris %>% as_tibble()
 test_data$Species = as.character(test_data$Species)
 
 
-#THIS IS WHERE DATA WRANGINGLING AND DATABASE CONNECTION STARTS
+#DATABASE CONNECTION STARTS
 #_______________________________________________________________________
-
 
 #DATA CLEANSING
 #_______________________________________________________________________
